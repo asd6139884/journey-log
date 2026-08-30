fb9823d2f628c0bda0324ddf9eb8f33ae12d0f49
ChihYi <asd6139884@gmail.com>
2026-08-30 20:27:31 +0800

    CORS add Cloudflare web

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://journey-log.asd6139884.workers.dev",
     ],
     allow_credentials=True,
     allow_methods=["*"],
