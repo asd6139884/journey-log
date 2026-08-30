c6e44e7bd2cea21a3715c482caa26d88eeea8ca1
ChihYi <asd6139884@gmail.com>
2026-08-30 21:14:47 +0800

    update EscapeRoomCard

--- frontend/src/components/EscapeRoomCard.tsx
+++ frontend/src/components/EscapeRoomCard.tsx
@@ -7,8 +7,9 @@ interface EscapeRoomCardProps {
   room: EscapeRoom;
 }
 
-
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = import.meta.env.DEV
+  ? "http://127.0.0.1:8000"
+  : "https://journey-log-331i.onrender.com";
 
 
 function EscapeRoomCard({
